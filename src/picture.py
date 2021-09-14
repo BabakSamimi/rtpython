4c979eaf93b4cd5ca9e40f7d736c5b77c9f07f58
Babak Samimi <babak@samimi.se>
2021-09-14 23:02:12 +0200

    Minor update: Fixed viewport bug. Still setting up the renderer, close to implementing the real deal

--- src/picture.py
+++ src/picture.py
@@ -3,13 +3,12 @@ import numpy as np
 
 WIDTH = 300
 HEIGHT = 200
+MARGIN = 200
 MAX_DEPTH = 2
 
-
-def fill_screen(screen_buffer):
-    for y in range(0, WIDTH):
-        for x in range(0, HEIGHT):
-            screen_buffer.set_at((x,y), pygame.Color(0,0,0))
+def normalize(vec):
+    # divides each component of vec with its length, normalizing it
+    return vec / np.linalg.norm(vec)
 
 class Ray:
     def __init__(self, origin, direction):
@@ -20,59 +19,92 @@ class Ray:
         # Coefficient is used to move the ray along a line in 3D space
         return origin + (coefficient*direction)
 
-# 3D object class
-class Mesh:
-    def __init__(self):
-        pass
+def ray_color(ray):
+    pass
+
+class Sphere:
+    def __init__(self, center, radius):
+        self.center = center # determines where in the 3D world the center of the sphere exists
+        self.radius = radius
+        #self.color = color
 
 class Camera:
-    def __init__(self, origin, viewport_width, viewport_height):
+    def __init__(self, origin, viewport_width, viewport_height, focal_length):
         self.origin = origin
         self.horizontal = np.array([viewport_width, 0, 0])
         self.vertical = np.array([0, viewport_height, 0])
-        
-        self.coordinate_origin = origin - horizontal/2 - vertical/2 - np.array([0, 0, 1])
+        self.focal_length = focal_length # distance between camera and viewport
+
+        self.lower_left_corner = self.origin - self.horizontal/2 - self.vertical/2 - np.array([0, 0, focal_length])
     
 def main():
     pygame.init()
 
-    
-    camera = np.array([0, 0, 1]) # camera starts at (0,0,1) in 3D-space
-    aspect_ratio = float(WIDTH/HEIGHT) # 3:2 aspect ratio
-    print("Aspect ratio:")
-    
+    aspect_ratio = float(WIDTH/HEIGHT) # 3:2 aspect ratio for now
+    print("Aspect ratio:", aspect_ratio)
+
     # we want the viewport to have the same aspect ratio as the image itself
     # the viewport ranges from -1 to 1 in the x-axis
-    # and -1/ratio to 1/ratio in the y-axis
-    viewport = (-1, 1 / aspect_ratio, 1, -1 / aspect_ratio) # LEFT, TOP, RIGHT, BOTTOM
+    # and -1/aspect_ratio to 1/aspect_ratio in the y-axis
+    # these numbers are really arbitrary, but as long as they have the same ratio as the image itself it's fine
+    
+    viewport_width = 1.0
+    viewport_height = (1.0 / aspect_ratio)
+    viewport = (-viewport_width, -viewport_height, viewport_width, viewport_height) # LEFT, BOTTOM, RIGHT, TOP
+
+    origin = np.array([0,0,1])
+    camera = Camera(origin, viewport_width, viewport_height, 1.0)
+
+    scene_objects = [ Sphere(np.array([-0.2, 0., -1]), 0.5), Sphere(np.array([0.1, -0.3, 0.]), 0.2)]
 
     # A pixel-array with 3 values for each pixel (RGB)
-    backbuffer = np.zeros((HEIGHT, WIDTH, 3))
-           
-    framebuffer = pygame.display.set_mode((WIDTH, HEIGHT))
+    # Essential this is a Width x Height with a depth of 3
+    # PyGame will read the backbuffer as [X, Y], hence why WIDTH is being used to determine the rows of the matrix
+    backbuffer = np.zeros((WIDTH, HEIGHT, 3))
+               
+    framebuffer = pygame.display.set_mode((WIDTH + MARGIN, HEIGHT + MARGIN))
     pygame.display.set_caption("Backwards ray-tracing")
-    
+        
     running = True
-    
+
+    # Init progress text
+    font = pygame.font.Font(None, 21)
+
+    render = True
+
     while running:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # enumerating through linspace will give us indices for the image itself
-        # and the y and x values will correspond to the viewport (screen-coordinate space)
-        for height_index, y in enumerate(np.linspace(viewport[1], viewport[3], HEIGHT)):
-            for width_index, x in enumerate(np.linspace(viewport[0], viewport[2], WIDTH)): 
-                backbuffer[height_index, width_index] = np.array([255, 30, 30])
+        if render:
+            # y_index and x_index are indices used for the pixel array and y and x are the viewport coordinates 
+            for y_index, y in enumerate(np.linspace(viewport[1], viewport[3], HEIGHT)):
+                for x_index, x in enumerate(np.linspace(viewport[0], viewport[2], WIDTH)):
+                    if y_index % 10 == 0:
+                        percentage = (y_index / WIDTH) * 100
+                        progress = "Progress: " + '{:.2f}'.format(percentage) + "%"
+                        text = font.render(progress, False, (255, 255, 255), (0,0,0))
+                        textpos = text.get_rect(centerx=MARGIN/2 + 60, centery=MARGIN/2 - 15)
+                        framebuffer.blit(text, textpos)
+                        pygame.display.update()
 
-        temp_framebuffer = pygame.surfarray.make_surface(backbuffer)
-        framebuffer.blit(temp_framebuffer, (0,0))
-        
-        # render update
-        pygame.display.update()
+
+                    pixel = np.array([x,y,0])
+                    direction = normalize(pixel - origin) # figure out where the ray is be going                 
+                    ray = Ray(origin, direction)
+                    
+                    y_dir = ray.direction[1]
+                    backbuffer[x_index, y_index] = np.array([np.abs(y_dir)*255, 30, 255])
+
+            temp_framebuffer = pygame.surfarray.make_surface(backbuffer)
+            framebuffer.blit(temp_framebuffer, ( (MARGIN/2, MARGIN/2) )) # second parameter centers our viewport
+
+            # render update
+            pygame.display.update()
+        render = False
 
     
-        
 if __name__ == '__main__':
     main()
