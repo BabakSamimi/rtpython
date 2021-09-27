77fa82944cc5de9c83b2790fbbf3f0504327bca5
Babak Samimi <babak@samimi.se>
2021-09-27 11:44:56 +0200

    Primitive WASD movement imlemented!

--- src/picture.py
+++ src/picture.py
@@ -1,11 +1,16 @@
 from time import perf_counter
 import pygame
+from pygame.locals import *
 import numpy as np
 import argparse
 
 from utility import *
 from classes import *
 
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
+
+# WIDTH AND HEIGHT ARE FOR THE TIME BEING USED AS DIMENSIONS FOR OUR VIEWPORT (WHICH THEN GETS SCALED UP)
 WIDTH = 300
 HEIGHT = 200
 INSET = 200
@@ -25,32 +30,32 @@ if args.height:
 # treat sphere intersection as a quadratic function to solve, f = ax^2 + bx + c
 # https://en.wikipedia.org/wiki/Quadratic_equation#Quadratic_formula_and_its_derivation
 def sphere_intersection(ray, sphere):
-    rD = ray.direction
-    rO = ray.origin
-    sC = sphere.center
-    sR = sphere.radius
-    rOsC = rO-sC # origin - sphere
-    unit_rOsC = normalize(rOsC)
+  rD = ray.direction
+  rO = ray.origin
+  sC = sphere.center
+  sR = sphere.radius
+  rOsC = rO-sC # origin - sphere
+  unit_rOsC = normalize(rOsC)
 
-    # determines if the ray direction is looking at the sphere, temporary code
-    if np.dot(unit_rOsC, rD) > 0:
-        return -1.0
+  # determines if the ray direction is looking at the sphere, temporary code
+  if np.dot(unit_rOsC, rD) > 0:
+    return -1.0
 
-    a = 1 # a = ||rD|| = 1 because rD is a unit vector
-    b = 2 * np.dot(rD, rOsC) # b = 2 * rD (rO - sC)
-    c = np.dot(rOsC, rOsC) - np.dot(sR, sR) # (rO - sC)^2 - sR^2, dot product with itself will square the vector
-    discriminant = (b**2) - (4*c)
+  a = 1 # a = ||rD|| = 1 because rD is a unit vector
+  b = 2 * np.dot(rD, rOsC) # b = 2 * rD (rO - sC)
+  c = np.dot(rOsC, rOsC) - np.dot(sR, sR) # (rO - sC)^2 - sR^2, dot product with itself will square the vector
+  discriminant = (b**2) - (4*c)
     
-    if discriminant < 0:
-        # no solutions
-        return -1.0
-    else:
-        # return closest intersection
-
-        d1 = (-b + np.sqrt(discriminant)) / 2
-        d2 = (-b - np.sqrt(discriminant)) / 2
-        if d1 > 0 and d2 > 0:
-            return min(d1, d2)          
+  if discriminant < 0:
+    # no solutions
+    return -1.0
+  else:
+    # return closest intersection
+    
+    d1 = (-b + np.sqrt(discriminant)) / 2
+    d2 = (-b - np.sqrt(discriminant)) / 2
+    if d1 > 0 and d2 > 0:
+      return min(d1, d2)          
           
 def plane_intersection(ray, plane):
     rD = ray.direction
@@ -107,6 +112,7 @@ def intersect_objects(ray, scene_objects, x,y):
         pZ = int(point[2])
 
           # for every other x and z position that is even, color the pixel white, otherwise gray/black
+          # might not work behind the camera
         if ((pX % 2 == 0) == (pZ % 2 == 0)):
           color = np.array([255,255,255])
         else:
@@ -115,12 +121,28 @@ def intersect_objects(ray, scene_objects, x,y):
 
     return color
 
+def process_input(keys, origin):
+
+  if keys[K_w]:
+    origin[2] -= 0.1
+  elif keys[K_a]:
+    origin[0] -= 0.1
+  elif keys[K_s]:
+    origin[2] += 0.1
+  elif keys[K_d]:
+    origin[0] += 0.1
+  else:
+    return (False, origin)
+  print(origin)
+  return (True, origin)
 
 def main():
     
     pygame.init()
 
-    aspect_ratio = float(WIDTH/HEIGHT) # 3:2 aspect ratio for now
+    print("Smooth scale backend:", pygame.transform.get_smoothscale_backend())
+
+    aspect_ratio = float(WIDTH/HEIGHT)
     print("Aspect ratio:", aspect_ratio)
 
     # Init font for progress text
@@ -141,14 +163,15 @@ def main():
     # PyGame will read the backbuffer as [X, Y], hence why WIDTH is being used to determine the rows of the matrix
     backbuffer = np.zeros((WIDTH, HEIGHT, 3))
                
-    framebuffer = pygame.display.set_mode((WIDTH + INSET, HEIGHT + INSET))
+#   framebuffer = pygame.display.set_mode((WIDTH + INSET, HEIGHT + INSET))
+    framebuffer = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Backwards ray-tracing")
 
     render = True
     running = True
 
     # ray origin
-    origin = np.array([0,0,1])
+    origin = np.array([0.0, 0.0, 1.0])
 
     # multi-array, first array is for spheres, second array for planes, third for lights
     scene_objects = []
@@ -164,10 +187,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+            if event.type == pygame.KEYDOWN:
+              render, origin = process_input(pygame.key.get_pressed(), origin)
+
+
         if render:
             start_counter = perf_counter()
-            
+            print(origin)
             # y_index and x_index are indices used for the pixel array and y and x are the viewport coordinates 
             for y_index, y in enumerate(np.linspace(viewport[1], viewport[3], HEIGHT)):
                 if (y_index+1) % 10 == 0:
@@ -190,7 +216,13 @@ def main():
 
             
             temp_framebuffer = pygame.surfarray.make_surface(backbuffer)
-            framebuffer.blit(temp_framebuffer, ( (INSET/2, INSET/2) )) # second parameter centers our viewport
+            #temp_framebuffer_upscaled = pygame.transform.scale(temp_framebuffer, (1200, 800))
+
+            # create an upscaled version of our frame that is half the size of the window dimensions
+            temp_framebuffer_upscaled = pygame.transform.smoothscale(temp_framebuffer, (int(WINDOW_WIDTH/2),int(WINDOW_HEIGHT/2)) )
+            center_x, center_y = (int(WINDOW_WIDTH/4), int((WINDOW_HEIGHT/4)))
+            
+            framebuffer.blit(temp_framebuffer_upscaled, ( (center_x, center_y )))
             pygame.display.update()
             
             end_counter = perf_counter()
